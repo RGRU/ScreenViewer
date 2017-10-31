688d51822da2fc397ca4144e55735917c3aa0fe0
Alexey Chernyshev <chernyshev@rg.ru>
2017-10-31 17:00:24 +0300

    add: module to git

---
+++ flow/libdef.js
@@ -0,0 +1,4 @@
+// declare var rg;
+// declare var emitter;
+declare var log;
+declare var logStyle;
---
+++ src/scripts/index.js
@@ -0,0 +1,49 @@
+import { Observable } from 'rxjs/Rx';
+import screenViewer from './modules/screenViewer';
+
+/**
+ * Список потоков, по изменению ширины страницы
+ * @type {Array}
+ */
+const targetEventList = [
+
+    /**
+     * Поток данных по событию полной загрузки страницы
+     * с фильтрацией корректной ширины
+     *
+     * @type {Rx}
+     */
+    Observable
+        .fromEvent(window, 'load')
+        .map(() => window.innerWidth),
+
+    /**
+     * Поток данных по событию DOMContentLoaded,
+     * когда весь DOM загружен
+     * с фильтрацией корректной ширины
+     *
+     * @type {Rx}
+     */
+    Observable
+        .fromEvent(document, 'DOMContentLoaded')
+        .map(event => event.target.innerWidth),
+
+    /**
+     * Поток данных по событию ресайза страницы
+     * с фильтрацией корректной ширины
+     *
+     * @type {Rx}
+     */
+    Observable
+        .fromEvent(window, 'resize')
+        .map(event => event.target.innerWidth)
+];
+
+/**
+ * Инициализируем модуль, сохраняя поток изменения типов экрана
+ * @type {Rx}
+ */
+let screen$ = screenViewer.init$(targetEventList);
+
+// Прослушиваем изменения
+screen$.subscribe(console.log);
